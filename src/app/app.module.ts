3a5d4b963223973a480ef409fb08a74ead62bea9
nipulviduranga <71004802+nipulviduranga@users.noreply.github.com>
2021-01-26 23:05:59 +0530

    1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,14 +10,25 @@ import {MatButtonModule} from '@angular/material/button';
 import {FormsModule} from '@angular/forms';
 import {RouterModule} from '@angular/router';
 import {MatIconModule} from '@angular/material/icon';
-import { PagesComponent } from './dashboard/pages/pages.component';
+
+import { DashBoardComponent } from './dash-board/dash-board.component';
+import { HomeComponent } from './dash-board/pages/home/home.component';
+import { NewsAndEventsComponent } from './dash-board/pages/news-and-events/news-and-events.component';
+import { TimeTablesComponent } from './dash-board/pages/time-tables/time-tables.component';
+import { MarksComponent } from './dash-board/pages/marks/marks.component';
+import { DefaultPageComponent } from './dash-board/pages/default-page/default-page.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginFormComponent,
-    PagesComponent
+    DashBoardComponent,
+    HomeComponent,
+    NewsAndEventsComponent,
+    TimeTablesComponent,
+    MarksComponent,
+    DefaultPageComponent
   ],
   imports: [
     BrowserModule,
