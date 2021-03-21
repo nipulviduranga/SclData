142edd51a7edb6f69271eae365c61e11902db266
nipulviduranga <71004802+nipulviduranga@users.noreply.github.com>
2021-03-21 07:52:40 +0530

    final8

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production:true,
- /* baseUrl1:'https://murmuring-brook-64774.herokuapp.com/api/v1/studentRoute/',
-  baseUrl2:'https://murmuring-brook-64774.herokuapp.com/api/v1/'*/
-  baseUrl1:'http://localhost:3001/api/v1/studentRoute/',
-  baseUrl2:'http://localhost:3001/api/v1/',
+  baseUrl1:'https://murmuring-brook-64774.herokuapp.com/api/v1/studentRoute/',
+  baseUrl2:'https://murmuring-brook-64774.herokuapp.com/api/v1/',
+  /*baseUrl1:'http://localhost:3001/api/v1/studentRoute/',
+  baseUrl2:'http://localhost:3001/api/v1/',*/
   UserAdmin:'admin',
   UserPassword:'admin'
 };
